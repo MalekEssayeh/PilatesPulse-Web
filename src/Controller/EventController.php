be2e207202b8dbe4052854da7199ab574b4f076f
Ben Abdallah Nada <benabdallahnada@isamm.u-manouba.tn>
2024-04-17 03:23:49 +0100

    backend

--- src/Controller/EventController.php
+++ src/Controller/EventController.php
@@ -14,6 +14,8 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/event')]
 class EventController extends AbstractController
 {
+
+    //front-end
     #[Route('/', name: 'app_event_index', methods: ['GET'])]
     public function index(EventRepository $eventRepository): Response
     {
@@ -78,4 +80,72 @@ class EventController extends AbstractController
 
         return $this->redirectToRoute('app_event_index', [], Response::HTTP_SEE_OTHER);
     }
+
+
+    //back-end
+    #[Route('/admin/index', name: 'app_event_index_admin', methods: ['GET'])]
+    public function indexAdmin(EventRepository $eventRepository): Response
+    {
+        return $this->render('backend/event/index.html.twig', [
+            'events' => $eventRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/admin/new', name: 'app_event_new_admin', methods: ['GET', 'POST'])]
+    public function newAdmin(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $event = new Event();
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($event);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_event_index_admin', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('backend/event/new.html.twig', [
+            'event' => $event,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/admin/{id}', name: 'app_event_show_admin', methods: ['GET'])]
+    public function showAdmin(Event $event): Response
+    {
+        return $this->render('/backend/event/show.html.twig', [
+            'event' => $event,
+        ]);
+    }
+
+    #[Route('/admin/{id}/edit', name: 'app_event_edit_admin', methods: ['GET', 'POST'])]
+    public function editAdmin(Request $request, Event $event, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_event_index_admin', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('backend/event/edit.html.twig', [
+            'event' => $event,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/admin/{id}', name: 'app_event_delete_admin', methods: ['POST'])]
+    public function deleteAdmin(Request $request, Event $event, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$event->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($event);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_event_index_admin', [], Response::HTTP_SEE_OTHER);
+    }
+
 }
--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -14,6 +14,7 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/session')]
 class SessionController extends AbstractController
 {
+    //front-end
     #[Route('/', name: 'app_session_index', methods: ['GET'])]
     public function index(SessionRepository $sessionRepository): Response
     {
@@ -77,5 +78,72 @@ class SessionController extends AbstractController
         }
 
         return $this->redirectToRoute('app_session_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+
+    //back-end
+    #[Route('/admin/index', name: 'app_session_index_admin', methods: ['GET'])]
+    public function indexAdmin(SessionRepository $sessionRepository): Response
+    {
+        return $this->render('backend/session/index.html.twig', [
+            'sessions' => $sessionRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/admin/new', name: 'app_session_new_admin', methods: ['GET', 'POST'])]
+    public function newAdmin(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $session = new Session();
+        $form = $this->createForm(SessionType::class, $session);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($session);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_session_index_admin', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('backend/session/new.html.twig', [
+            'session' => $session,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/admin/{id}', name: 'app_session_show_admin', methods: ['GET'])]
+    public function showAdmin(Session $session): Response
+    {
+        return $this->render('backend/session/show.html.twig', [
+            'session' => $session,
+        ]);
+    }
+
+    #[Route('/Admin/{id}/edit', name: 'app_session_edit_admin', methods: ['GET', 'POST'])]
+    public function editAdmin(Request $request, Session $session, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(SessionType::class, $session);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_session_index_admin', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('backend/session/edit.html.twig', [
+            'session' => $session,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/admin/{id}', name: 'app_session_delete_admin', methods: ['POST'])]
+    public function deleteAdmin(Request $request, Session $session, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$session->getId(), $request->request->get('_token'))) {
+            $entityManager->remove($session);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_session_index_admin', [], Response::HTTP_SEE_OTHER);
     }
 }
