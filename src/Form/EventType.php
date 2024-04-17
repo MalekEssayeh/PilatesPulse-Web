6282003369e5d1d3d009e5e53498a8818a85d608
Ben Abdallah Nada <benabdallahnada@isamm.u-manouba.tn>
2024-04-17 06:36:45 +0100

    input validation

--- src/Form/EventType.php
+++ src/Form/EventType.php
@@ -7,22 +7,70 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\LessThan;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\Constraints\Positive;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class EventType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('date')
-            ->add('nbrParticipants')
-            ->add('description')
-            ->add('coach_id')
+            ->add('name', null, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Name cannot be blank.']),
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z0-9\s]+$/',
+                        'message' => 'The name should only contain letters and numbers.',
+                    ]),
+                ],
+            ])
+            ->add('date', null, [
+                'constraints' => [
+                    new NotNull(['message' => 'Name cannot be blank.']),
+                    new GreaterThan([
+                        'value' => 'today',
+                        'message' => 'The date must be after today.',
+                    ]),
+                ],
+            ])
+            ->add('nbrParticipants', null, [
+                'constraints' => [
+                    new NotNull(['message' => 'Name cannot be blank.']),
+                    new Positive(),
+                    new LessThan(['value' => 100, 'message' => 'The number of participants must be less than 100.']),
+                ],
+            ])
+            ->add('description', null, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Name cannot be blank.']),
+                ],
+            ])
+            ->add('coach_id', null, [
+                'constraints' => [
+                    new NotNull(['message' => 'Name cannot be blank.']),
+                ],
+            ])
             ->add('imageUrl', FileType::class, [
                 'label' => 'Image (JPEG or PNG file)',
                 'required' => false,
                 'mapped' => true,
                 'attr' => ['accept' => 'image/jpeg, image/png'],
+                'constraints' => [
+                    new File([
+                        'maxSize' => '5M',
+                        'maxSizeMessage' => 'The file is too large ({{ size }} {{ suffix }}). Maximum size allowed is {{ limit }} {{ suffix }}.',
+                        'mimeTypes' => [
+                            'image/jpeg',
+                            'image/png',
+                        ],
+                        'mimeTypesMessage' => 'Please upload a valid JPEG or PNG image file.',
+                    ]),
+                ],
             ]);
     }
 
--- src/Form/SessionType.php
+++ src/Form/SessionType.php
@@ -6,23 +6,63 @@ use App\Entity\Event;
 use App\Entity\Session;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TimeType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\GreaterThan;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Positive;
+use Symfony\Component\Validator\Constraints\Regex;
 
 class SessionType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('name')
-            ->add('description')
-            ->add('duration')
-            ->add('coach_id')
+            ->add('name', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Name cannot be blank.']),
+                    new Regex([
+                        'pattern' => '/^[a-zA-Z0-9\s]+$/',
+                        'message' => 'The name should only contain letters (capital or not) and numbers.',
+                    ]),
+                ],
+            ])
+            ->add('description', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Description cannot be blank.']),
+                ],
+            ])
+            ->add('duration', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Duration cannot be blank.']),
+                    new Positive(['message' => 'Duration must be a positive number.']),
+                    new GreaterThan([
+                        'value' => 0,
+                        'message' => 'Duration must be greater than 0.',
+                    ]),
+                ],
+            ])
+            ->add('coach_id', TextType::class, [
+                'constraints' => [
+                    new NotBlank(['message' => 'Coach ID cannot be blank.']),
+                ],
+            ])
             ->add('event', EntityType::class, [
                 'class' => Event::class,
                 'choice_label' => 'name',
+                'constraints' => [
+                    new NotBlank(['message' => 'Event cannot be blank.']),
+                ],
             ])
-            ->add('time')
+            ->add('time', TimeType::class, [
+                'widget' => 'single_text',
+                'constraints' => [
+                    new NotBlank(['message' => 'Time cannot be blank.']),
+                ],
+            ])
         ;
     }
 
